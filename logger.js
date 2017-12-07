8d199155e256cd5abfe9b4b1cb16b064b98ad742
LGBitencourt <lg.bitencourt@gmail.com>
2017-12-07 21:00:44 +0100

    Correcting date format on log file

--- logger.js
+++ logger.js
@@ -11,25 +11,35 @@ const dateFormat = require('dateformat');
  * For more details: https://github.com/winstonjs/winston/tree/2.x
  */
 
+function getDate() {
+  return dateFormat(new Date(), "UTC:[dd/mm/yy HH:MM]");
+}
+
+function getFormatter(options) {
+  date = getDate();
+  level = options.level.toUpperCase();
+  if (options.colorize)
+    level = winston.config.colorize(options.level, level)
+  return date + ' ' + level + ': ' +
+    (options.message ? options.message : '') +
+    (options.meta && Object.keys(options.meta).length ? '\n'+ JSON.stringify(options.meta) : '');
+}
+
 const logger = new winston.Logger({
   transports: [
     new winston.transports.Console({
-      timestamp: function() {
-        return Date.now();
-      },
       level: "debug",
       handleExceptions: true,
       json: false,
       colorize: true,
-      formatter: function(options) {
-        date = dateFormat(new Date(options.timestamp()), 'yyyy-mm-dd hh:mm');
-        return '[' + date + ']' + ' ' +
-          winston.config.colorize(options.level, options.level.toUpperCase()) + ' ' +
-          (options.message ? options.message : '') +
-          (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '');
-      }
+      formatter: getFormatter
     }),
-    new (winston.transports.File)({ filename: 'run.log', json: false })
+    new (winston.transports.File)({
+      filename: 'run.log', 
+      json: false,
+      colorize: false,
+      formatter: getFormatter
+    })
   ]
 });
 
